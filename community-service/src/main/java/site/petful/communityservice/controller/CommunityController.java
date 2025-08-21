ae59b4532cc65e9340431ad409b8434a653ab965
LimHub <dla6963@naver.com>
2025-08-21 17:42:13 +0900

    fix:유저서비스 수정 풀 받기전 커밋

--- community-service/src/main/java/site/petful/communityservice/controller/CommunityController.java
+++ community-service/src/main/java/site/petful/communityservice/controller/CommunityController.java
@@ -3,6 +3,7 @@ package site.petful.communityservice.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import site.petful.communityservice.common.ApiResponse;
@@ -11,6 +12,8 @@ import site.petful.communityservice.common.ErrorCode;
 import site.petful.communityservice.dto.PostCreateRequest;
 import site.petful.communityservice.dto.PostDto;
 import site.petful.communityservice.service.CommunityService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("community")
@@ -21,15 +24,22 @@ public class CommunityController {
 
     @PostMapping("/register")
     public ApiResponse<Void> newRegistration(
-            @RequestHeader(value = "X-User-No",   required = false) Long userNo,
-            @RequestHeader(value = "X-User-Role", required = false) String role,
+            @RequestHeader(value = "X-User-N0",   required = false) Long userNo,
+            @RequestHeader(value = "X-User-Type", required = false) String userType,
             @RequestBody PostCreateRequest request
     ) {
         if(userNo == null){
             return ApiResponseGenerator.fail(ErrorCode.INVALID_REQUEST,"해당 유저가 존재하지 않습니다.");
         }
-        PostDto dto = communityService.registNewPost(userNo, request);
+        communityService.registNewPost(userNo, request);
         return ApiResponseGenerator.success();
+    }
+    @GetMapping("/debug-headers")
+    public Map<String, String> debugHeaders(@RequestHeader HttpHeaders headers) {
+        return Map.of(
+                "HDR_USER_NO", headers.getFirst("HDR_USER_NO"),
+                "HDR_USER_TYPE", headers.getFirst("HDR_USER_TYPE")
+        );
     }
 
 }
