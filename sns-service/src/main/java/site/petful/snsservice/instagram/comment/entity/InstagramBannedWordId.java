6884404a9c0fa8f7977a71af332c7e75c9cceb55
gyongcode <gyongcode@gmail.com>
2025-08-26 20:54:06 +0900

    refactor: 구조 변경

--- sns-service/src/main/java/site/petful/snsservice/instagram/comment/entity/InstagramBannedWordId.java
+++ sns-service/src/main/java/site/petful/snsservice/instagram/comment/entity/InstagramBannedWordId.java
@@ -1,4 +1,21 @@
 package site.petful.snsservice.instagram.comment.entity;
 
-public class InstagramBannedWordId {
+
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class InstagramBannedWordId implements Serializable {
+
+    private Long instagramId;
+    private String word;
+
 }
