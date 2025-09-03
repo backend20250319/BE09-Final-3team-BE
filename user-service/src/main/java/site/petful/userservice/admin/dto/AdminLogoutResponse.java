bb288ab6849d76c892d08f86da32837cff49a551
dkrio <dkrio34@gmail.com>
2025-09-03 10:04:00 +0900

    feat : 관리자 로그아웃 기능 추가

---
+++ user-service/src/main/java/site/petful/userservice/admin/dto/AdminLogoutResponse.java
@@ -0,0 +1,16 @@
+package site.petful.userservice.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminLogoutResponse {
+    private String message;
+    private Long adminId;
+    private String adminType;
+}
