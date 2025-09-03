2de025934999a1dd611d942ea05b36407cf07793
dkrio <dkrio34@gmail.com>
2025-09-03 12:37:47 +0900

    fix : admin 충돌 부분 해결

--- user-service/src/main/java/site/petful/userservice/admin/controller/UserAdminController.java
+++ user-service/src/main/java/site/petful/userservice/admin/controller/UserAdminController.java
@@ -1,7 +1,6 @@
 package site.petful.userservice.admin.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
