424b97792f439d311d89416696516b9e429a74f3
LimHub <dla6963@naver.com>
2025-09-05 21:58:15 +0900

    fix: 체험단 알림 발생시, 개인 한명에게만 발생되는 오류 수정

--- notification-service/src/main/java/site/petful/notificationservice/security/FeignAuthConfig.java
+++ notification-service/src/main/java/site/petful/notificationservice/security/FeignAuthConfig.java
@@ -1,5 +1,6 @@
 package site.petful.notificationservice.security;
 
+
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
