11112f08f445b26aa2fd2f723b7e62ed66356a91
LimHub <dla6963@naver.com>
2025-09-05 19:16:24 +0900

    fix:EventMessage dto Target -> List<Target> 으로 수정

--- notification-service/src/main/java/site/petful/notificationservice/dto/EventMessage.java
+++ notification-service/src/main/java/site/petful/notificationservice/dto/EventMessage.java
@@ -1,11 +1,14 @@
 package site.petful.notificationservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -18,7 +21,9 @@ public class EventMessage {
     private String type;            // ex) notification.comment.created
     private Instant occurredAt;
     private Actor actor;
-    private Target target;
+    @JsonProperty("target")
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    private List<Target> target;
     private Map<String, Object> attributes;
     private Integer schemaVersion;
 
