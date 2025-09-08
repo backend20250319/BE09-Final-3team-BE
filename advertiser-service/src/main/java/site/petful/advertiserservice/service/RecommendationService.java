c3499dd9742ee21b09dc34b41ac9d99aade62dd8
NYoungLEE <skdud0694@gmail.com>
2025-09-09 01:59:43 +0900

    feat: 펫스타 추천 API 생성

--- advertiser-service/src/main/java/site/petful/advertiserservice/service/RecommendationService.java
+++ advertiser-service/src/main/java/site/petful/advertiserservice/service/RecommendationService.java
@@ -1,26 +1,34 @@
 package site.petful.advertiserservice.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import site.petful.advertiserservice.dto.advertisement.AdResponse;
 
 @Service
 public class RecommendationService {
 
     private final WebClient webClient;
+    private final AdService adService;
 
-    public RecommendationService(WebClient.Builder webClientBuilder) {
+    public RecommendationService(WebClient.Builder webClientBuilder, AdService adService) {
         this.webClient = webClientBuilder
                 .baseUrl(System.getenv()
                 .getOrDefault("GATEWAY_URL", "http://localhost:8000"))
                 .build();
+        this.adService = adService;
     }
 
     // 펫스타 추천
-    public Mono<String> getPetStars(String token) {
-        return webClient.get()
+    public Mono<String> getPetStars(String token, Long adNo) {
+
+        AdResponse adInfo = adService.getAd(adNo);
+
+        return webClient.post()
                 .uri("/recommendation/hello") // 이후 pets로 수정
                 .header("Authorization", token)
+                .body(BodyInserters.fromValue(adInfo))
                 .retrieve()
                 .bodyToMono(String.class);
     }
